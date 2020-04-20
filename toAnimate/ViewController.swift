ac4e25e6d0e583c9148081a4774630827c73c300
Yeh, Jamie <jamie.yeh@disney.com>
2020-04-19 19:47:41 -0700

    initialize project

---
+++ toAnimate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toAnimate
+//
+//  Created by Yeh, Jamie on 4/18/20.
+//  Copyright © 2020 JW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
